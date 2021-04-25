8c4f78de07058427343e7828346509d1c2122559
AndresViracacha <viracfandres@gmail.com>
2021-04-24 23:28:12 -0500

    se arreglo el header

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,8 @@
-$(".submenu").click(function () {
-  $(this).children("ul").slideToggle("600");
-});
-$("ul").click(function (ev) {
-  ev.stopPropagation();
-});
+if (window.innerWidth < 998) {
+  $(".submenu").click(function () {
+    $(this).children("ul").slideToggle("600");
+  });
+  $("ul").click(function (ev) {
+    ev.stopPropagation();
+  });
+}
